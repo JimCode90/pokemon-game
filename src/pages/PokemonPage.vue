07c0ccb4ea64571066283ae268a08ba7d1c9075a
JimCode <jimmypaytan10@gmail.com>
2022-07-28 16:58:07 -0500

    seccion 7 - video 90

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>¿Quien es este pokemon?</h1>
+    <PokemonPicture :pokemonId="6" :showPokemon="true" />
+    <PokemonOptions />
+  </div>
+</template>
+
+<script>
+
+import PokemonPicture from "@/components/PokemonPicture";
+import PokemonOptions from "@/components/PokemonOptions"
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonPicture, PokemonOptions
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
